97a94939c76e431d9d5a7eced771dabfa9df36b8
Manish Kumar <m-bakshi@hotmail.com>
2020-01-22 23:00:46 +0530

    added motor control

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,6 +1,7 @@
 // Listen for orientation changes
 var land = $('#landscape-view');
 var port = $('#portrait-view');
+var socket = io(); //load socket.io-client and connect to the host that serves the page
 
 function selectView(){
 	//console.log(screen);
@@ -18,3 +19,45 @@ selectView();
 window.addEventListener("orientationchange", function() {
 	selectView();
 }, false);
+
+window.addEventListener("load", function(){ //when page loads
+  if(screen.width > 850){
+	  var leftSliderL = document.getElementById("left-slider-l");
+	  leftSliderL.addEventListener("change", function() { //add event listener for when checkbox changes
+		socket.emit("leftSlider", Number(this.value)); //send button status to server (as 1 or 0)
+	  });
+	  var rightSliderL = document.getElementById("right-slider-l");
+	  rightSliderL.addEventListener("change", function() { //add event listener for when checkbox changes
+		socket.emit("rightSlider", Number(this.value)); //send button status to server (as 1 or 0)
+	  });		 
+  }else{
+	  var leftSliderP = document.getElementById("left-slider-p");
+	  leftSliderP.addEventListener("change", function() { //add event listener for when checkbox changes
+		socket.emit("leftSlider", Number(this.value)); //send button status to server (as 1 or 0)
+	  });
+	  var rightSliderP = document.getElementById("right-slider-p");
+	  rightSliderP.addEventListener("change", function() { //add event listener for when checkbox changes
+		socket.emit("rightSlider", Number(this.value)); //send button status to server (as 1 or 0)
+	  });
+  }
+});
+
+socket.on('leftSlider', function (data) { //get button status from client
+  if(screen.width > 850){
+	  document.getElementById("left-slider-l").value = data;	
+      socket.emit("leftSlider", data);	  
+  }else{
+	  document.getElementById("left-slider-p").value = data;
+	  socket.emit("leftSlider", data);
+  }
+});
+
+socket.on('rightSlider', function (data) { //get button status from client
+  if(screen.width > 850){
+	  document.getElementById("right-slider-l").value = data;	
+      socket.emit("rightSlider", data);	  
+  }else{
+	  document.getElementById("right-slider-p").value = data;
+	  socket.emit("rightSlider", data);
+  }
+});
--- webserver.js
+++ webserver.js
@@ -1,5 +1,14 @@
 var http = require('http').createServer(handler); //require http server, and create server with function handler()
 var fs = require('fs'); //require filesystem module
+var io = require('socket.io')(http) //require socket.io module and pass the http object (server)
+const Gpio = require('pigpio').Gpio;
+
+const motor1PWM = new Gpio(17, {mode: Gpio.OUTPUT});
+const motor1CA = new Gpio(27, {mode: Gpio.OUTPUT});
+const motor1CB = new Gpio(22, {mode: Gpio.OUTPUT});
+const motor2PWM = new Gpio(18, {mode: Gpio.OUTPUT});
+const motor2CA = new Gpio(23, {mode: Gpio.OUTPUT});
+const motor2CB = new Gpio(24, {mode: Gpio.OUTPUT});
 
 http.listen(8080); //listen to port 8080
 
@@ -15,8 +24,19 @@ function handler (req, res) { //create server
 		res.write(data); //write data from index.html
 		return res.end();
 	  });
-  }else if(req.url.endsWith(".css")){
+  }else if(req.url.endsWith("bootstrap.min.css")){
 	  fs.readFile(__dirname + '/public/css/bootstrap.min.css', function(err, data) { 
+		if (err) {
+		  res.writeHead(404, {'Content-Type': 'text/html'}); //display 404 on error
+		  return res.end("404 Not Found");
+		} 
+		
+		res.writeHead(200, {'Content-Type': 'text/css'}); 
+		res.write(data); 
+		return res.end();
+	  });
+  }else if(req.url.endsWith("custom.css")){
+	  fs.readFile(__dirname + '/public/css/custom.css', function(err, data) { 
 		if (err) {
 		  res.writeHead(404, {'Content-Type': 'text/html'}); //display 404 on error
 		  return res.end("404 Not Found");
@@ -48,6 +68,17 @@ function handler (req, res) { //create server
 		res.write(data); 
 		return res.end();
 	  });
+  }else if(req.url.endsWith("socket.io.js")){
+	  fs.readFile(__dirname + '/public/js/socket.io.js', function(err, data) { 
+		if (err) {
+		  res.writeHead(404, {'Content-Type': 'text/html'}); //display 404 on error
+		  return res.end("404 Not Found");
+		} 
+		
+		res.writeHead(200, {'Content-Type': 'text/javascript'}); 
+		res.write(data); 
+		return res.end();
+	  });
   }else if(req.url.endsWith("custom.js")){
 	  fs.readFile(__dirname + '/public/js/custom.js', function(err, data) { 
 		if (err) {
@@ -61,3 +92,45 @@ function handler (req, res) { //create server
 	  });
   }
 }
+
+io.sockets.on('connection', function (socket) {// WebSocket Connection
+  var leftValue = 0; //static variable for current status
+  var rightValue = 0; //static variable for current status
+  socket.on('leftSlider', function(data) { //get leftSlider status from client
+    leftValue = data;
+	if(leftValue > 255){
+		leftValue = (255-(511-leftValue));
+		motor1CA.digitalWrite(0);
+		motor1CB.digitalWrite(1);
+	}else{
+		leftValue = (255-leftValue);
+		motor1CA.digitalWrite(1);
+		motor1CB.digitalWrite(0);
+	}
+    motor1PWM.pwmWrite(leftValue);
+  });
+  
+  socket.on('rightSlider', function(data) { //get leftSlider status from client
+    rightValue = data;
+	if(rightValue > 255){
+		rightValue = (255-(511-rightValue));
+		motor2CA.digitalWrite(0);
+		motor2CB.digitalWrite(1);
+	}else{
+		rightValue = (255-rightValue);
+		motor2CA.digitalWrite(1);
+		motor2CB.digitalWrite(0);
+	}
+    motor2PWM.pwmWrite(rightValue);
+  });
+});
+
+process.on('SIGINT', function () { //on ctrl+c
+  motor1PWM.pwmWrite(0);
+  motor1CA.digitalWrite(0);
+  motor1CB.digitalWrite(0);
+  motor2PWM.pwmWrite(0);
+  motor2CA.digitalWrite(0);
+  motor2CB.digitalWrite(0);
+  process.exit(); //exit completely
+});
